65eeeaa45a0da3ec3ab83a7e1295fc9383635adb
Samuel <116453375+TheSolobaid@users.noreply.github.com>
2023-01-19 08:32:56 +0100

    Update main.py

--- loto/main.py
+++ loto/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from test import testnbr, testchoice, tirage , testseed, testsave, testname, testmoyen, testrecherche
+from test import testnbr, testchoice, tirage , testseed, testsave, testname, testmoyen, testrecherche, testsorted
 from search import rechercheDichotomiqueIteratif, rechercheDichotomiqueRecursive
 from tri import TriCocktail , triFusion , triInsertion
 from exportSAVE import exportCSV, exportJSON, exportPICKLE
@@ -9,38 +9,40 @@ from histogram import histogram
 
 
 while True:
-    nbr,test,seed,save,choice,format,sorting,way,recher,moyen,recherche='a','a','a','a','a','a','a','a','a','a','a'           #déclaration de valeur fausse pour pouvoir rentrer dans les boucles de test
+    nbr,test,seed,save,choice,format,sorting,way,recher,moyen,recherche,name,charge='a','a','a','a','a','a','a','a','a','a','a','none','a'          #déclaration de valeur fausse pour pouvoir rentrer dans les boucles de test
     iteration = 0
     chargedata = False
     sorted = False
 
 
             #chargement de donnée
-    charge = int(input("voulez vous charger des données déjà générées? (1 = oui / 0 = non)? ")) 
-    if charge == 1:                                                                                     
+    while not testchoice(charge):
+        charge = input("voulez vous charger des données déjà générées? (1 = oui / 0 = non)? ")
+        if not testchoice(charge):
+            print("ceci n'est pas une valeur acceptable (0/1)\n")
+    if int(charge) == 1:                                                                                     
         chargedata = False
         while not testsave(format):
             format = input("quel est le format d'enregistrement de vos données? (.csv = 1 / .json = 2 / .pkl = 3)? ")
             if not testsave(format):
                 print("ceci n'est pas une valeur acceptable (1/2/3)\n")
         if int(format) == 1:    
-            print('on va test CSV')
             name = input('nom de la sauvegarde: ')
             tir = importCSV(name)
             chargedata = True
         if int(format) == 2:    
-            print('on va test JSON')
             name = input('nom de la sauvegarde: ')
             tir = importJSON(name)
             chargedata = True
         if int(format) == 3:   
-            print('ona va test pickle')
             name = input('nom de la sauvegarde: ')
             tir = importPICKLE(name)
             chargedata = True
         format = 'a'
+        if testsorted(name):
+            sorted = True
         print(tir)
-    
+
 
     if not chargedata:
             #génération de donnée si besoin
@@ -66,41 +68,42 @@ while True:
 
 
         #test sorting data
-    while not testchoice(sorting):
-        sorting = input("\n--------------------------------------\n\nvoulez vous trier vos données? (oui = 1 / non = 0)? ")
-        if not testchoice(sorting):
-            print("ceci n'est pas une valeur acceptable (0/1)\n")
-    if int(sorting) == 1:
-        while not testsave(way):
-            way = input("de quelle manière? (1 = tri Cocktail / 2 = tri Fusion / 3 = tri insertion)? ")
-            testsave(way)
-            if not testsave(way):
-                print("ceci n'est pas une valeur acceptable (1/2/3)\n")
-        if int(way) == 1:                                                                                                                        #tri cocktail
-            tritotal = []
-            for i in range(len(tir)):
-                templist=tir.iloc[i].values.tolist()
-                templist = TriCocktail(templist)
-                tritotal.append(templist)
-            tir =  pd.DataFrame(tritotal, columns=['1st nbr','2nd nbr','3rd nbr','4th nbr','5th nbr'])
-            sorted = True
-        if int(way) == 2:                                                                                                                        #tri fusion
-            tritotal = []
-            for i in range(len(tir)):
-                templist=tir.iloc[i].values.tolist()
-                templist = triFusion(templist)
-                tritotal.append(templist)
-            tir =  pd.DataFrame(tritotal, columns=['1st nbr','2nd nbr','3rd nbr','4th nbr','5th nbr'])
-            sorted = True
-        if int(way) == 3:                                                                                                                        #tri insertion
-            tritotal = []
-            for i in range(len(tir)):
-                templist=tir.iloc[i].values.tolist()
-                templist = triInsertion(templist)
-                tritotal.append(templist)
-            tir =  pd.DataFrame(tritotal, columns=['1st nbr','2nd nbr','3rd nbr','4th nbr','5th nbr'])
-            sorted = True
-        print(tir)
+    if not sorted:
+        while not testchoice(sorting):
+            sorting = input("\n--------------------------------------\n\nvoulez vous trier vos données? (oui = 1 / non = 0)? ")
+            if not testchoice(sorting):
+                print("ceci n'est pas une valeur acceptable (0/1)\n")
+        if int(sorting) == 1:
+            while not testsave(way):
+                way = input("de quelle manière? (1 = tri Cocktail / 2 = tri Fusion / 3 = tri insertion)? ")
+                testsave(way)
+                if not testsave(way):
+                    print("ceci n'est pas une valeur acceptable (1/2/3)\n")
+            if int(way) == 1:                                                                                                                        #tri cocktail
+                tritotal = []
+                for i in range(len(tir)):
+                    templist=tir.iloc[i].values.tolist()
+                    templist = TriCocktail(templist)
+                    tir.iloc[i]=templist
+                tir =  pd.DataFrame(tritotal, columns=['1st nbr','2nd nbr','3rd nbr','4th nbr','5th nbr'])
+                sorted = True
+            if int(way) == 2:                                                                                                                        #tri fusion
+                tritotal = []
+                for i in range(len(tir)):
+                    templist=tir.iloc[i].values.tolist()
+                    templist = triFusion(templist)
+                    tir.iloc[i]=templist
+                tir =  pd.DataFrame(tritotal, columns=['1st nbr','2nd nbr','3rd nbr','4th nbr','5th nbr'])
+                sorted = True
+            if int(way) == 3:                                                                                                                        #tri insertion
+                tritotal = []
+                for i in range(len(tir)):
+                    templist=tir.iloc[i].values.tolist()
+                    templist = triInsertion(templist)
+                    tir.iloc[i]=templist
+                tir =  pd.DataFrame(tritotal, columns=['1st nbr','2nd nbr','3rd nbr','4th nbr','5th nbr'])
+                sorted = True
+            print(tir)
 
 
         #sauvegarde de donnée
@@ -114,26 +117,23 @@ while True:
             if not testsave(format):
                 print("ceci n'est pas une valeur acceptable (1/2/3)\n")
         if int(format) == 1:    
-            name = input('nom de la sauvegarde: ')
-            '''while not testname(name, format):
+            while not testname(name, format):
+                name = input('nom de la sauvegarde: ')
                 if not testname(name, format):
                     print(f"{name} est déjà le nom d'une sauvegarde, veuillez changer de nom \n")
-                name = input('nom de la sauvegarde: ')'''
-            exportCSV(tir, name)
+            exportCSV(tir, name, sorted)
         if int(format) == 2:    
-            name = input('nom de la sauvegarde: ')
-            """while not testname(name,format):
+            while not testname(name,format):
+                name = input('nom de la sauvegarde: ')
                 if not testname(name,format):
                     print(f"{name} est déjà le nom d'une sauvegarde, veuillez changer de nom \n")
-                name = input('nom de la sauvegarde: ')"""
-            exportJSON(tir, name)
+            exportJSON(tir, name,sorted)
         if int(format) == 3:    
-            name = input('nom de la sauvegarde: ')
-            """while not testname(name,format):
+            while not testname(name,format):
+                name = input('nom de la sauvegarde: ')
                 if not testname(name,format):
                     print(f"{name} est déjà le nom d'une sauvegarde, veuillez changer de nom \n")
-                name = input('nom de la sauvegarde: ')"""
-            exportPICKLE(tir, name)
+            exportPICKLE(tir, name,sorted)
 
 
         #choix pour histogramme
@@ -147,7 +147,7 @@ while True:
     
         #choix recherche
     while not testchoice(recher):
-            recher = int(input("\n--------------------------------------\n\nvoulez vous rechercher un nombre précis? (oui = 1 / non = 0)? "))
+            recher = input("\n--------------------------------------\n\nvoulez vous rechercher un nombre précis? (oui = 1 / non = 0)? ")
             if not testchoice(recher):
                 print("ceci n'est pas une valeur acceptable (0/1)\n")
     if sorted:
@@ -169,16 +169,16 @@ while True:
                 if iteration == 0:
                     print(f"{recherche} n'est pas présent dans la liste")
                 else:
-                    print(f"{recherche} est {iteration} fois dans le(s) {len(tir)+1} tirage(s)")
+                    print(f"{recherche} est {iteration} fois dans le(s) {len(tir)} tirage(s)")
             if int(moyen) == 2:
-                for i in range(5):
+                for i in range(len(tir)):
                     templist=tir.iloc[i].values.tolist()
-                    if rechercheDichotomiqueRecursive(templist, recherche, 0, len(templist)):
+                    index = rechercheDichotomiqueRecursive(templist, recherche)
+                    if index is not False:
                         iteration += 1
-                        print(f"le nombre {recherche} est dans le {i+1}e tirage à la {rechercheDichotomiqueRecursive(templist, recherche, 0, len(templist))+1}e place\n")
+                        print(f"le nombre {recherche} est dans le {i+1}e tirage à la {index+1}e place\n")
                 if iteration == 0:
                     print(f"{recherche} n'est pas présent dans la liste")
-                else:
-                    print(f"{recherche} est {iteration} fois dans le(s) {len(tir)+1} tirage(s)")
+                print(f"{recherche} est {iteration} fois dans le(s) {len(tir)} tirage(s)")
     else:
         print("la recherche ne peut pas être effectuée car la liste n'est pas triée! ")
