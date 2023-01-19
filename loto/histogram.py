e759eae7a1fa1c1437dec8d6f8142a919e806e9c
Samuel <116453375+TheSolobaid@users.noreply.github.com>
2023-01-19 08:34:49 +0100

    Update histogram.py

--- loto/histogram.py
+++ loto/histogram.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
-l=[]
-for i in range(45):
-    l.append(i+1)
-def histogram(dF):
-    plt.hist(dF, bins=45, facecolor='g')
-    plt.xticks(l)
-    plt.xlabel('valleur')
-    plt.ylabel("nbr de fois qu'il apparêt")
-    plt.title('Histogram')
+import numpy as np
+
+def histogram(df):
+    fréquence = df.stack().value_counts()
+    plt.bar(fréquence.index, fréquence.values)
+    plt.xticks(np.arange(1,46))
+    plt.xlabel('Nombre')
+    plt.ylabel('fréquence')
     plt.show()
