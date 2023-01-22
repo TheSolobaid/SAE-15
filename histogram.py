987f7bd8d270a97ff7253b79b7f0a8845a629f0c
TheSolobaid <thesolobaid@gmail.com>
2023-01-22 18:13:34 +0100

    SAE15 Final drop

--- histogram.py
+++ histogram.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def histogramme(df):
-    histo = {} 
+    valuelist = {} 
     for colonne in df.columns: 
         for valeur in df[colonne]: 
-            if valeur in histo:
-                histo[valeur] += 1 
+            if valeur in valuelist:
+                valuelist[valeur] += 1 
             else:
-                histo[valeur] = 1 
-    plt.bar(histo.keys(), histo.values()) 
+                valuelist[valeur] = 1 
+    plt.bar(valuelist.keys(), valuelist.values()) 
     plt.xticks(np.arange(1,46))
     plt.xlabel('Nombre')
     plt.ylabel('fréquence')
